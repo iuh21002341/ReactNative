c9e4937ba979fb5afdd1af7abd3386e5ae36c12c
Vieque <lier19vel@gmail.com>
2024-08-21 10:32:10 +0700

    Ex09

---
+++ Lab01/ex09.js
@@ -0,0 +1,7 @@
+// 9. Create a function that will convert from Fahrenheit to Celsius 
+
+function fahrenheitToCelsius(fahrenheit) {
+	return (fahrenheit - 32) * 5/9;
+}
+
+console.log(fahrenheitToCelsius(32)); // 0
