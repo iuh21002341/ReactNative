290a05d1f486d67059d2159b932254c68373811f
Vieque <lier19vel@gmail.com>
2024-08-21 10:31:47 +0700

    Ex08

---
+++ Lab01/ex08.js
@@ -0,0 +1,8 @@
+// 8. Create a function that will convert from Celsius to Fahrenheit 
+// (Celsius * 9/5) + 32
+
+function celsiusToFahrenheit(celsius) {
+	return (celsius * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0)); // 32
