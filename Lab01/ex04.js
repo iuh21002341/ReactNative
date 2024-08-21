7c8f7b82df4cfaf0eacd39570e2d64139da56f94
Vieque <lier19vel@gmail.com>
2024-08-21 10:29:30 +0700

    Ex04

---
+++ Lab01/ex04.js
@@ -0,0 +1,7 @@
+// 4. Print all the multiplication tables with numbers from 1 to 10 
+
+for (let i = 1; i <= 10; i++) {
+	for (let j = 1; j <= 10; j++) {
+		console.log(`${i} * ${j} = ${i * j}`);
+	}
+}
