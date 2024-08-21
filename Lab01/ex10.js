b69e101ec20cee22285518a5c236cab74cfaefcc
Vieque <lier19vel@gmail.com>
2024-08-21 10:33:45 +0700

    Ex10

---
+++ Lab01/ex10.js
@@ -0,0 +1,9 @@
+// 10. Calculate the sum of numbers in an array of numbers 
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+	sum += numbers[i];
+}
+console.log(sum);
