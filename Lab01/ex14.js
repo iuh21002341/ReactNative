7d6b6044fab3926e48cd5321edaa02c5b2ff9fdf
Vieque <lier19vel@gmail.com>
2024-08-21 10:47:04 +0700

    Ex14

---
+++ Lab01/ex14.js
@@ -0,0 +1,12 @@
+// 14. Print the first 10 Fibonacci numbers without recursion 
+
+let n = 10;
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+for (let i = 1; i <= n; i++) {
+	console.log(n1);
+	nextTerm = n1 + n2;
+	n1 = n2;
+	n2 = nextTerm;
+}
