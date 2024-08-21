0242b279f796034d7e1433ced18bc8ee184aaa4c
Vieque <lier19vel@gmail.com>
2024-08-21 10:52:17 +0700

    Ex17

---
+++ Lab01/ex17.js
@@ -0,0 +1,16 @@
+// 17. Calculate the sum of digits of a positive integer number 
+
+function sumOfDigits(n) {
+	
+	let sum = 0;
+
+	while (n > 0) {
+		sum += n % 10;
+		n = Math.floor(n / 10);
+	}
+
+	return sum;
+
+}
+
+console.log(sumOfDigits(123)); // 6
